d709c2598b40a35b952bad5f6fd73b9ea6542b63
klooj <57016271+klooj@users.noreply.github.com>
2021-02-10 22:40:22 -0500

    Update nvim.lua

--- lua/nlua/lsp/nvim.lua
+++ lua/nlua/lsp/nvim.lua
@@ -19,7 +19,10 @@ local nlua_nvim_lsp = {
     "%s/nlua/sumneko_lua/lua-language-server/",
     cache_location
   ),
-  bin_location = base_directory .. "bin/" .. bin_folder .. "/lua-language-server"
+  bin_location = string.format(
+    "%s/nlua/sumneko_lua/lua-language-server/bin/%s/lua-language-server",
+    cache_location,
+    bin_folder
 }
 
 local sumneko_command = function()
