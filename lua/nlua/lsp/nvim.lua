e9b4a02613aea0346e3ae79f482682cd6d76bccd
kludgeon <viaEmail_j@protonmail.com>
2021-02-10 20:52:28 -0500

    build foo

--- lua/nlua/lsp/nvim.lua
+++ lua/nlua/lsp/nvim.lua
@@ -10,11 +10,11 @@ local bin_folder = function()
   end
 end
 
-if g.is_mac then
-  build_foo = '/usr/local/share/nvim/runtime'
-else
-  build_foo = g.home .. 'build/neovim/src/nvim'
-end
+-- if g.is_mac then
+  -- build_foo = '/usr/local/share/nvim/runtime'
+-- else
+build_foo = g.home .. 'build/neovim/src/nvim'
+-- end
 
 local nlua_nvim_lsp = {
   base_directory = string.format(
